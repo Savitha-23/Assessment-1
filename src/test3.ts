0804f69371a0f7e18c6f5c406976bc160af24157
Savitha-23 <savi.ek05@gmail.com>
2026-06-29 11:13:22 +0530

    Test3Completed

---
+++ src/test3.ts
@@ -0,0 +1,227 @@
+// // Question: 1
+
+// const studentName: string = "Rahul";
+// const department: string = "Computer Science";
+
+// let tamil: number = 85;
+// let english: number = 78;
+// let mathematics: number = 92;
+// let science: number = 88;
+// let social: number = 76;
+
+// let totalMarks: number = 
+// tamil +
+// english +
+// mathematics +
+// science +
+// social;
+
+// let average: number = totalMarks / 5;
+
+// console.log("Student Name :", studentName);
+// console.log("Department :", department);
+// console.log("Total Marks :", totalMarks);
+// console.log("Average :", average);
+
+// if (average >= 80 && totalMarks >= 400) {
+//     console.log("Grade : A");
+// } else {
+//     console.log("Grade : B");
+// }
+
+// average = average + 2;
+// console.log("Updated Average :", average);
+
+// if (average > 90) {
+//     console.log("Outstanding Performance");
+
+// } else {
+//     console.log("Good Performance");
+
+// }
+// console.log("Assessment Completed");
+
+
+// // Question: 2
+
+// let employeeName: string = "Alex";
+// let EmpID: number = 1001;
+// let depart: string = "Software Testing";
+
+// let basicSalary: number = 25000;
+// let hra: number = 5000;
+// let bonus: number = 3000;
+// let tax: number = 2000;
+
+// let grossSalary: number = basicSalary + hra + bonus;
+// let netSalary: number = grossSalary - tax;
+
+// console.log("Employee Name :", employeeName);
+// console.log("EmpID :", EmpID);
+// console.log("depart :", depart);
+// console.log("basicSalary :", basicSalary);
+// console.log("hra :", hra);
+// console.log("bonus :", bonus);
+// console.log("tax :", tax);
+
+// if (netSalary >= 30000) {
+//     console.log("Promotion Status : Eligible");
+
+// } else {
+//     console.log("Promotion Status : Not Eligible");
+// }
+
+// netSalary = netSalary + 5000;
+
+// console.log("Salary After Increment : ", netSalary);
+
+// if (netSalary >= 35000) {
+//     console.log("Performance Grade : A");
+// } else {
+//     console.log("Performance Grade : B");
+// }
+
+// let experience: number = 5;
+
+// console.log("Experience :", experience);
+
+// if (experience >= 10) {
+//     console.log("Senior Employee");
+// } else {
+//     console.log("Junior Employee");
+// }
+
+// let attendance: number = 100;
+
+// console.log("Attendance :", attendance);
+
+// if (attendance >= 75) {
+//     console.log("Attendance Status : Pass");
+
+// } else {
+//     console.log("Attendance Status : Fail");
+// }
+
+// console.log("Employee Details Updated Successfully");
+
+// Question: 3
+
+// const companyName: string = "OpenAI";
+// const employeeID: number = 101;
+// const basicSalary: number = 28000;
+// const hra: number = 4000;
+// const medicalAllowance: number = 2000;
+
+// const grossSalary: number = basicSalary + hra + medicalAllowance;
+
+// console.log("Company Name :", companyName);
+// console.log("employeeID :", employeeID);
+// console.log("Salary :", basicSalary);
+// console.log("HRA :", hra);
+// console.log("Medical Allowance :", medicalAllowance);
+// console.log("Gross Salary :", grossSalary);
+
+// if (grossSalary >= 35000) {
+//     console.log("Salary Grade : A");
+// } else {
+//     console.log("Salary Grade : B");
+// }
+
+// const employee = {
+//     id: employeeID,
+//     name: "Alex",
+//     salary: grossSalary,
+// };
+
+// console.log("Employee ID :", employee.id);
+// console.log("Employee Name :", employee.name);
+// console.log("Current Salary :", employee.salary);
+
+// employee.salary = employee.salary + 3000;
+
+// console.log("Updated Salary :", employee.salary);
+
+// const annualSalary = employee.salary * 12
+// console.log("Annual Salary :", annualSalary);
+
+// if (annualSalary >= 450000){
+//     console.log("Bonus : Eligible");
+// } else {
+//     console.log("Bonus : Not Eligible");
+// }
+
+// const tax = 3700;
+// console.log("Tax Amount :", tax);
+
+// const netSalary = 33300;
+// console.log("Net Salary :", netSalary);
+
+// if (netSalary >= 34000) {
+//     console.log("Performance Rating : Excellent");
+// } else {
+//     console.log("Performance Rating : Good");
+// }
+
+// console.log("Final Employee Salary :", 33300);
+// console.log("Interview Completed");
+
+
+// Question 4: 
+
+// const employeeName: string = "Alex";
+// let totalSalary: number = 30000;
+// let completedTasks: number = 0;
+// let totalBonus: number = 0;
+// const workingDays: number = 5;
+
+// console.log("Employee :", employeeName);
+
+// for (let day = 1; day <= workingDays; day++){
+//     console.log("Working Day :", day);
+
+// completedTasks = completedTasks + 2;
+
+// console.log("Completed Tasks :", completedTasks);
+
+// if (completedTasks >= 6){
+//     totalBonus = totalBonus + 1000;
+//     console.log("Daily Bonus : 1000");
+
+// } else {
+//     totalBonus = totalBonus + 500;
+//     console.log("Daily Bonus : 500");
+
+// }
+// }
+// console.log("Total Bonus :", totalBonus);
+// totalSalary = totalSalary + totalBonus;
+
+// if (
+//     totalSalary >= 35000 && completedTasks >= 10
+// ) {
+//     console.log("Performance : Excellent");
+// } else {
+//     console.log("Performance : Good");
+// }
+// console.log("Interview Success");
+
+// Question: 5
+
+const employeeName: string = "Alex";
+const monthlyTarget: number = 60000;
+const monthlySales: number[] = [
+    12000,
+    9000,
+    18000,
+    7000,
+    15000,
+    11000
+];
+
+let totalSales: number = 72000;
+let incentive: number = 6000;
+let penalty: number = 1000;
+let highSalesDays: number = 4;
+let bestSales: number = 0;
+let commission: number = 3600;
+console.log(" 5th Question Unable to solve so many confusions need assistance");
